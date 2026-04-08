85d2df9856f8636d4decb2088e6a898c2c9ba17f
rochi88 <raisul.me@gmail.com>
2026-04-08 23:21:56 +0600

    wip

--- Pi_MCP23S17_Port Expander/button_gpio_expander_test.py
+++ Pi_MCP23S17_Port Expander/button_gpio_expander_test.py
@@ -6,20 +6,20 @@ try:
     mcp1 = MCP23S17(ce=1)
     mcp1.open()
 
-    for x in range(0, 16):
+    for x in range(0, 15):
         mcp1.setDirection(x, mcp1.DIR_INPUT)
         mcp1.setPullupMode(x, mcp1.PULLUP_ENABLED)
 
     old_btn = 1
 
-    print "Starting reading pin A0 (CTRL+C to quit)"
+    print("Starting reading pin A0 (CTRL+C to quit)")
     while (True):
         new_btn = mcp1.digitalRead(0)
         if new_btn != old_btn:
             if new_btn == 0:
-                print "Button pressed!"
+                print("Button pressed!")
             else:
-                print "Button released!"
+                print("Button released!")
 
         old_btn = new_btn
 
